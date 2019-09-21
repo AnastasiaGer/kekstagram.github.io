7e4dcc77ca6913c7aa99c190e92ba8c24ce54b1e
Anisa <aagermanova@gmail.com>
2019-09-20 22:50:27 -0700

    Начала писать код

---
+++ js/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var usersPhotos = [];
+var similarListElement = document.querySelector('.pictures');
+var similarPhotoTemplateElement = document.querySelector('#picture').content.querySelector('.picture__link');
