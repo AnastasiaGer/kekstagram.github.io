ab225a362f687ab85b85befee381854959a1b729
Anisa <aagermanova@gmail.com>
2019-10-03 11:01:27 -0700

    Исправила закрытие большой фото по Esc

--- js/picture.js
+++ js/picture.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+
+  var MODAL_OPEN_CLASS = 'modal-open';
   var hideItem = function (item) {
     item.classList.add('visually-hidden');
   };
@@ -8,17 +10,32 @@
   var bigPictureElement = document.querySelector('.big-picture');
   var bigPictureCancelElement = bigPictureElement.querySelector('.big-picture__cancel');
 
+  var closeModal = function () {
+    document.body.classList.remove(MODAL_OPEN_CLASS);
+    window.utils.hideElement(bigPictureElement);
+  };
+
   document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === window.utils.isKeydownEsc) {
-      document.body.classList.remove('modal-open');
-      window.utils.hideElement(bigPictureElement);
-    }
+    window.utils.isKeydownEsc(evt, closeModal);
   });
 
   bigPictureCancelElement.addEventListener('click', function () {
-    document.body.classList.remove('modal-open');
-    window.utils.hideElement(bigPictureElement);
+    closeModal();
   });
+
+  var createComments = function () {
+    var comment = document.createElement('li');
+    comment.classList.add('social__comment');
+    comment.classList.add('social__text');
+    var img = document.createElement('img');
+    img.classList.add('social__picture');
+    img.src = 'img/avatar-' + window.utils.getRandomPoint(1, 6) + '.svg';
+    img.alt = 'Автор комментария';
+    img.width = 35;
+    img.height = 35;
+    comment.appendChild(img);
+    return comment;
+  };
 
   var showBigPhoto = function (data) {
     window.utils.showElement(bigPictureElement);
@@ -34,20 +51,6 @@
   };
 
   showBigPhoto(window.gallery.photos[0]);
-
-  var createComments = function () {
-    var comment = document.createElement('li');
-    comment.classList.add('social__comment');
-    comment.classList.add('social__text');
-    var img = document.createElement('img');
-    img.classList.add('social__picture');
-    img.src = 'img/avatar-' + window.utils.getRandomPoint(1, 6) + '.svg';
-    img.alt = 'Автор комментария';
-    img.width = 35;
-    img.height = 35;
-    comment.appendChild(img);
-    return comment;
-  };
 
   window.picture = {
     showBigPhoto: showBigPhoto,
